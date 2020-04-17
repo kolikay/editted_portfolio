d1b7cc49358d2844fdda955849323b6a406c990a
kolikay <kolikay1989@yahoo.com>
2020-04-17 01:28:12 +0100

    worked on experince, resume and skills component

--- src/portfolio/Contact.js
+++ src/portfolio/Contact.js
@@ -1,9 +1,61 @@
 import React, { Component } from "react"
+import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl'
 
 class Contact extends Component{
     render(){
         return(
-                <div><h1>Contact Page</h1></div>
+                <div className='contact-body'>
+                    <Grid className='contact-grid'>
+                         <Cell col={6}>
+                             <h2>Kolawole Fabusuyi</h2>
+                             <img 
+                             src="https://avatars2.githubusercontent.com/u/56051616?s=460&u=3f9305f057d67f7c505cffacb8e69786da78aab5&v=4"
+                             alt='avatar'
+                             style={{height: '250px'}}
+                             />
+                            <p>
+                            I'm new to Git and using it for the very first time. I would appreciate it if someone could help me out. I tried finding the answer at forums, but there are tons of commands that are coming out and not sure which one to use
+                            </p>
+                         </Cell>
+                         <Cell col={6}>
+                             <h2>Contact Me</h2>
+                             <hr/>
+                             <div className='contact-list'>
+                             <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily:"Anton"}}>
+                                <i className='fa fa-phone-square' aria-hidden='true' />
+                                (234)806-284-2978
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily:"Anton"}}>
+                                <i className='fa fa-envelope' aria-hidden='true' />
+                                kolikay1989@gmail.com
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily:"Anton"}}>
+                                <i className='fa fa-skype' aria-hidden='true' />
+                                kolikay
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily:"Anton"}}>
+                                <i className="fa fa-address-book-o" aria-hidden='true' />
+                                19, usman balogun street, ile-epo busstop, igando
+                                </ListItemContent>
+                            </ListItem>
+                           
+                            </List>
+
+                             </div> 
+                         </Cell>
+                    </Grid>
+                </div>
             )
     }
        
--- src/portfolio/Resume.js
+++ src/portfolio/Resume.js
@@ -1,9 +1,93 @@
 import React, { Component } from "react"
+import {Grid, Cell} from 'react-mdl'
+import Education from './Education'
+import Experience from './Experience'
+import Skills from './Skills'
 
 class Resume extends Component{
     render(){
         return(
-                <div><h1>Resume Page</h1></div>
+                <div>
+                    <Grid>
+                        <Cell col={4}>
+                            <div style={{textAlign: 'center'}}>
+                                <img 
+                                src="https://avatars2.githubusercontent.com/u/56051616?s=460&u=3f9305f057d67f7c505cffacb8e69786da78aab5&v=4"
+                                 alt='avatar'
+                                 style={{height:'200px'}}
+                                /></div>
+                                <h2 style={{paddingTop: '2em'}}>Kolawole Fabusuyi</h2>
+                                <h4 style={{color:'grey'}}>Web Developer</h4>
+                                <hr style={{borderTop: '3px solid #833fb2', width: '70%' }}/>
+                                <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book</p>
+                                <hr style={{borderTop: '3px solid #833fb2', width: '70%' }}/>
+                                <h5>Address</h5>
+                                <p>1, fabusuyi street ketu lagos</p>
+                                <h5>Phone</h5>
+                                <p>08062842978</p>
+                                <h5>Email</h5>
+                                <p>kolikay1989@yahoo.com</p>
+                                <h5>Website</h5>
+                                <p>www.kolikay.com</p>
+                                <hr style={{borderTop: '3px solid red', width: '70%' }}/>
+                        </Cell>
+                        <Cell col={8} className='resume-right-col'>
+                            <h2>Education</h2>
+
+                            <Education 
+                            startYear='2000'
+                            endYear='2005'
+                            schoolName='University of Abuja'
+                            schoolDesc='Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book'
+                            />
+
+                            <Education 
+                            startYear='2006'
+                            endYear='2010'
+                            schoolName='Code camp'
+                            schoolDesc='Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book'
+                            />
+
+                            <hr style={{borderTop: '3px solid red', width: '70%' }}/>
+                            <h2>Experience</h2>
+
+                            <Experience 
+                            startYear='2006'
+                            endYear='2010'
+                            jobName='Code camp'
+                            jobDesc='Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book'
+                            />
+
+                            <Experience 
+                            startYear='2010'
+                            endYear='2018'
+                            jobName='Code camp1'
+                            jobDesc='Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book'
+                            />
+                            <hr style={{borderTop: '3px solid red', width: '70%' }}/>
+                            <h2>Skills</h2>
+                            <Skills 
+                            skill = 'javascript'
+                            progress = {100}
+                            />
+                            <Skills 
+                            skill = 'python'
+                            progress = {75}
+                            />
+                            <Skills 
+                            skill = 'django'
+                            progress = {75}
+                            />
+                            <Skills 
+                            skill = 'HTML/CSS'
+                            progress = {80}
+                            />
+                             
+
+
+                        </Cell>
+                    </Grid>
+                </div>
             )
     }
        
