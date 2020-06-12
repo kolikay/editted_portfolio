435bed5000bb48e96b59ae6c5320eccab77e428e
kolikay <kolikay1989@yahoo.com>
2020-06-12 03:14:10 +0100

    debugging

--- src/portfolio/Footer.js
+++ src/portfolio/Footer.js
@@ -7,29 +7,29 @@ const Footer = () => {
     
         <div className='row' style={{backgroundColor:'black', 
                                  color:'rgb(226, 197, 29)',
-                                 paddingTop:'70px',
-                                 paddingLeft:'20px',
+                                 paddingTop:'10px',
+                                 paddingLeft:'10px',
                                  }}>
                 <div className='col-sm-12 col-md-6 col-lg-3'>
                     <i className="fa fa-address-book" aria-hidden="true" style={{fontSize:'30px'}}></i>
-                    <h5 style={{marginTop:'10px', marginBottom:'10px'}}>Address</h5>
-                    <p style={{color:'white'}}>No 9, Usman balogun street, adeson-ile epo street, igando akensan</p>
+                    <span style={{marginTop:'5px', marginBottom:'5px'}}>Address</span>
+                    <p style={{color:'white'}}>No 9, Usman balogun street, adeson-ile epo </p>
                 </div> 
                 
                 <div className='col-sm-12 col-md-6 col-lg-3' >
                 <i className="fa fa-envelope-o" aria-hidden="true" style={{fontSize:'30px'}}></i>
-                    <h5 style={{marginTop:'10px', marginBottom:'10px'}}>Email</h5>
+                    <span style={{marginTop:'5px', marginBottom:'5px'}}>Email</span>
                     <p style={{color:'white'}}>kolikay1989@gmail.com</p>
                 </div> 
                 
                 <div className=' col-sm-12 col-md-6 col-lg-3' >
-                <i className="fa fa-phone-square" aria-hidden="true" style={{fontSize:'30px'}}></i>
-                    <h5 style={{marginTop:'10px', marginBottom:'10px'}}>Call Us</h5>
+                <i className="fa fa-phone-square" aria-hidden="true" style={{fontSize:'30px'}}></i><span style={{marginTop:'5px', marginBottom:'5px'}}>Call Us</span>
+                    
                     <p style={{color:'white'}}>08062842978</p>
                 </div> 
                 
                 <div className='col-sm-12 col-md-6 col-lg-3' >
-                    <h5 style={{marginTop:'10px', marginBottom:'10px'}}>Social media</h5>
+                    <span style={{marginTop:'5px', marginBottom:'5px'}}>Social media</span>
                     <p>
                         <i style={{paddingRight:'30px', fontSize:'30px'}} className="fa fa-facebook-square" aria-hidden="true"></i>
                         <i style={{paddingRight:'30px', fontSize:'30px'}} className="fa fa-twitter-square" aria-hidden="true"></i>
