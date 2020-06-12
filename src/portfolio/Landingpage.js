e770d8e5217c38f9cc34b41f955f5d2bf27866f3
kolikay <kolikay1989@yahoo.com>
2020-06-12 01:37:27 +0100

    more bugs

--- src/portfolio/Landingpage.js
+++ src/portfolio/Landingpage.js
@@ -3,27 +3,27 @@ import HomePageDetails from './HomePageDetails'
 import { Link } from 'react-router-dom'
 
 
+
 class LandingPage extends Component {
 
 
     render() { 
-        return ( <Fragment >
-                    <div className='homepage' >
-                    
-                        <div className='container-fluid'>
-                            <div className='row'>
+        return ( 
+                    <div>
+                            <div className='row homepage'>
                                     <div className='col-12'>
                                         <h2>Kolawole Fabusuyi</h2>
                                         <h4>Welcome to my portfolio page, really glad you are here </h4>
                                         <div className='btn btn-lg btn-secondary'><Link to='/aboutme' style={{textDecoration:'none'}}> Read More > </Link></div>
                                     </div>
-                                </div>
                             </div>
-                   
-
+                            <div>
+                                <HomePageDetails />
+                            </div>
                     </div>
-                    <HomePageDetails />
-                    </Fragment>
+
+                    
+                    
                 
                 
                
