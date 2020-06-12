54931d5b4cd6205e2b196a65d9c0f4096ea66dcc
kolikay <kolikay1989@yahoo.com>
2020-06-12 02:14:08 +0100

    debugging

--- src/portfolio/Landingpage.js
+++ src/portfolio/Landingpage.js
@@ -10,10 +10,10 @@ class LandingPage extends Component {
     render() { 
         return ( 
                     <div >
-                            <div className='homepage'>
+                            <div className='row'>
                                     <div className='col-12'>
-                                        <h2>Kolawole Fabusuyi</h2>
-                                        <h4>Welcome to my portfolio page, really glad you are here </h4>
+                                        <h2 style={{color:'rgb(226, 197, 29)', paddingTop:'20%', fontFamily:'cursive'}}>Kolawole Fabusuyi</h2>
+                                        <h4 style={{color:'white', fontFamily:'cursive'}}>Welcome to my portfolio page, really glad you are here </h4>
                                         <div className='btn btn-lg btn-secondary'><Link to='/aboutme' style={{textDecoration:'none'}}> Read More > </Link></div>
                                     </div>
                             </div>
