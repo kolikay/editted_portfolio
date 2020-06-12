4cba0f3ce24b7675cf8fca40801d0dbb6da09673
kolikay <kolikay1989@yahoo.com>
2020-06-12 16:18:22 +0100

    re designing

--- src/portfolio/Footer.js
+++ src/portfolio/Footer.js
@@ -2,45 +2,40 @@ import React from 'react';
 
 
 const Footer = () => {
-    return ( <div className='container-fluid'>
+    return (    <div className='container-fluid' style={{backgroundColor:'black', 
+    color:'white'
+    }}>
 
-    
-        <div className='row' style={{backgroundColor:'black', 
-                                 color:'rgb(10, 62, 71)',
-                                 paddingTop:'10px',
-                                 paddingLeft:'10px',
-                                 }}>
-                <div className='col-sm-12 col-md-6 col-lg-3'>
-                    <i className="fa fa-address-book" aria-hidden="true" style={{fontSize:'30px'}}></i>
-                    <span style={{marginTop:'5px', marginBottom:'5px'}}>Address</span>
-                    <p style={{color:'white'}}>No 9, Usman balogun street, adeson-ile epo </p>
-                </div> 
-                
-                <div className='col-sm-12 col-md-6 col-lg-3' >
-                <i className="fa fa-envelope-o" aria-hidden="true" style={{fontSize:'30px'}}></i>
-                    <span style={{marginTop:'5px', marginBottom:'5px'}}>Email</span>
-                    <p style={{color:'white'}}>kolikay1989@gmail.com</p>
-                </div> 
-                
-                <div className=' col-sm-12 col-md-6 col-lg-3' >
-                <i className="fa fa-phone-square" aria-hidden="true" style={{fontSize:'30px'}}></i><span style={{marginTop:'5px', marginBottom:'5px'}}>Call Us</span>
+
+
+
+
+                   <div className='row'>
                     
-                    <p style={{color:'white'}}>08062842978</p>
-                </div> 
-                
-                <div className='col-sm-12 col-md-6 col-lg-3' >
-                    <span style={{marginTop:'5px', marginBottom:'5px'}}>Social media</span>
-                    <p>
-                        <i style={{paddingRight:'30px', fontSize:'30px'}} className="fa fa-facebook-square" aria-hidden="true"></i>
-                        <i style={{paddingRight:'30px', fontSize:'30px'}} className="fa fa-twitter-square" aria-hidden="true"></i>
-                        <i style={{paddingRight:'30px', fontSize:'30px'}} className="fa fa-google" aria-hidden="true"></i>
-                    
-                    </p>
+                        <div className='d-flex justify-content-center col-sm-12 col-md-12 col-lg-12' >
+                            <span style={{marginTop:'20px', marginBottom:'20px', fontSize:'20px'}}>Social media</span>
+                        </div>
+                    </div>
+
+                    <div className='footer'>
+               
+                    <span className=" fa fa-facebook-square" aria-hidden="true"></span>
+                    <span className="fa fa-twitter-square" aria-hidden="true"></span>
+                    <span className="fa fa-google" aria-hidden="true"></span>
+                    <span className="fa fa-linkedin" aria-hidden="true"></span>
+                    </div>
+
+
+
                 </div>
-
-            </div>
-        </div>
-     );
+                
+            );
 }
  
 export default Footer;
+{/* <p className='d-flex justify-content-center col-sm-12 col-md-6 col-lg-3'>
+<i className=" fa fa-facebook-square" aria-hidden="true"></i>
+<i className="fa fa-twitter-square" aria-hidden="true"></i>
+<i className="fa fa-google" aria-hidden="true"></i>
+
+</p> */}
