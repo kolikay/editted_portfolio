4de719c8d85915ab1060cb07ae7cf6114ea5e71a
kolikay <kolikay1989@yahoo.com>
2020-06-23 20:32:20 +0100

    fixing bugs

--- src/portfolio/Nav.js
+++ src/portfolio/Nav.js
@@ -1,5 +1,4 @@
 import React, {Fragment } from 'react';
-import { Link } from 'react-router-dom'
 import '../App.css'
 
 
@@ -9,7 +8,7 @@ const Nav =() =>{
     return(
         <Fragment >
                 <nav className="navbar navbar-expand-lg mb-0 navbar-dark  ">
-                <a className="navbar-brand" to="/" style={{fontSize:'40px', color:'white', fontFamily: 'Balsamiq Sans', marginRight:'auto'}}> Kolikay</a>
+                <a className="navbar-brand" href="/" style={{fontSize:'40px', color:'white', fontFamily: 'Balsamiq Sans', marginRight:'auto'}}> Kolikay</a>
                 
                 <button  className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
